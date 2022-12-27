d1cbbdb5701ec50080639dab87d52ca07d1adc3f
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2022-12-28 01:17:26 +0400

    Variables

---
+++ src/DataTypes2/MyFirstPractise2.java
@@ -0,0 +1,13 @@
+package DataTypes2;
+
+public class MyFirstPractise2 {
+    public static void main(String [] args){
+        int telebeSayi = 15;
+        String mesaj = "Tələbə sayı: ";
+
+        System.out.println(mesaj + telebeSayi);
+        System.out.println(mesaj + telebeSayi);
+        System.out.println("Tələbə sayım: "+telebeSayi);
+        System.out.println("Tələbə sayı: "+telebeSayi);
+    }
+}
