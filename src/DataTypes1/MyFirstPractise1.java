3f42852d16104e90505f48c5551dae45a4a04231
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2022-12-27 19:21:32 +0400

    Data Types

---
+++ src/DataTypes1/MyFirstPractise1.java
@@ -0,0 +1,32 @@
+package DataTypes1;
+
+public class MyFirstPractise1 {
+    public static void main (String [] args){
+        int a = 5;
+        int b = 7;
+
+        int c = a+b;
+        System.out.println(c);
+
+        int d = a-b;
+        System.out.println(d);
+
+        int e = a*b;
+        System.out.println(e);
+
+        double x = 15;
+        double y = 6;
+
+        double division = x/y;
+        System.out.println(division);
+
+        boolean response = true;
+        System.out.println(response);
+
+        char z = 'z';
+        System.out.println(z);
+
+        String word = "Hello World!";
+        System.out.println(word);
+    }
+}
