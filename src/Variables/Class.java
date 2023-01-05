18e63009ead214484a51373ea2f3c3f761e9ff60
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-01-05 20:14:52 +0400

    Static Variables 2

---
+++ src/Variables/Class.java
@@ -0,0 +1,14 @@
+package Variables;
+
+import javax.xml.namespace.QName;
+
+public class Class {
+    public static String studentName = "Wednesday Addams";
+    public static int age = 16;
+    public static void main(String[] args) {
+        System.out.println(Class.studentName);
+        System.out.println(Class.age);
+    }
+
+
+}
