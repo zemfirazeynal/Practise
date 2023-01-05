5b8b3e4d4726f9dee6c5ac08108efef0ef46a3cd
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-01-05 20:03:00 +0400

    Static Variables

---
+++ src/Variables/StaticVariable.java
@@ -0,0 +1,9 @@
+package Variables;
+
+public class StaticVariable {
+    public static String StaticVariable = "Static Variable";
+
+    public static void main (String [] args){
+        System.out.println(Variables.StaticVariable.StaticVariable);
+    }
+}
