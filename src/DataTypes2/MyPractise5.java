3c11556226d4983a06c6d4682798341f7f6c392b
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-01-11 22:50:41 +0400

    Operators

---
+++ src/DataTypes2/MyPractise5.java
@@ -0,0 +1,26 @@
+package DataTypes2;
+
+public class MyPractise5 {
+    public static void main (String [] args){
+
+        int a = 4 ;
+
+        a++; // 5
+        System.out.println(a);
+
+        a--; // 4
+        System.out.println(a);
+
+        a+=2; // a=a+2 deməkdir yəni vahid artır
+        System.out.println(a);
+
+        a-=5; // a=a-5 deməkdir yəni 5 vahid azalır
+        System.out.println(a);
+
+
+
+
+
+
+    }
+}
