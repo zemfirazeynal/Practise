587bdc48ce624eed8c0a7d0cd377928448d4b372
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-01-11 23:11:11 +0400

    Operators 2

---
+++ src/DataTypes2/MyPractise6.java
@@ -0,0 +1,33 @@
+package DataTypes2;
+
+public class MyPractise6 {
+    public static void main(String[] args) {
+        // / və %
+
+        int a = 75;
+
+        int b = a/10;
+        System.out.println(b); // 7
+
+        double c = a/10;
+        System.out.println(c); // 7.0
+
+        double c1 = (double) a /10;
+        System.out.println(c1); // 7.3
+
+
+        double d = 73;
+
+        double c2 = d/10;
+        System.out.println(c2); // 7.3
+
+
+        int e = 85;
+
+        int f = e%10;
+        System.out.println(f); // 5
+
+
+
+    }
+}
---
+++ src/DataTypes2/MyPractise7.java
@@ -0,0 +1,31 @@
+package DataTypes2;
+
+public class MyPractise7 {
+    public static void main(String[] args) {
+        int a = 53;
+        int tam = a/10; // 5
+        System.out.println("tam="+tam);
+
+        int qaliq = a%10; // 3
+        System.out.println("qaliq="+qaliq);
+
+        System.out.println("-------------");
+
+        double b = 51;
+
+        double tam2 = b/10; // 5.1
+        System.out.println("tam="+tam2);
+
+        int tam3 = (int) b/10; // 5
+        System.out.println("tam="+tam3);
+
+        double qaliq2 = b%10; // 1.0
+        System.out.println("qaliq="+qaliq2);
+
+        int qaliq3 = (int) b%10; // 1
+        System.out.println("qaliq="+qaliq3);
+
+
+
+    }
+}
