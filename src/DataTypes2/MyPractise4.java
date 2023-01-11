53f2d13887fa4720bb75cf6b8a8acc58a744104a
zemfirazeynal <109147342+zemfirazeynal@users.noreply.github.com>
2023-01-11 22:24:48 +0400

    Data Types 3

---
+++ src/DataTypes2/MyPractise4.java
@@ -0,0 +1,45 @@
+package DataTypes2;
+
+public class MyPractise4 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 2;
+
+        int toplama = a+b;
+        int cixma = a-b;
+        int vurma = a*b;
+        int bolme1 = a/b; // 2
+
+        double bolme2 = (double)a/b;
+
+        double bolme3 = a/(double)b;
+
+        double a2 = 5;  // ən qısa yol
+        double b2 = 2;
+
+        double bolme4 = a2/b2;
+
+        System.out.println("toplama="+(a+b));
+        System.out.println("toplama="+toplama);
+
+        System.out.println("çıxma="+cixma); // 3
+
+        System.out.println("vurma="+vurma); // 10
+
+        System.out.println("bölmə="+bolme1); // 2
+
+        System.out.println((double) bolme1); // 2.0
+
+        System.out.println(bolme2); // 2.5
+
+        System.out.println(bolme3); // 2.5
+
+        System.out.println(bolme4); // 2.5
+
+
+
+
+
+    }
+}
